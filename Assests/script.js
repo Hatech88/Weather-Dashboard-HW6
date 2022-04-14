c0b8096f9bcef3f30cb33270b669ddd98926083e
Harris Alshareef <hatech88@gmail.com>
2022-04-14 17:59:33 -0400

    Created the HTML and CSS file. Also used the bootstrap

---
+++ Assests/script.js
@@ -0,0 +1,7 @@
+var searchFromEl = document.querySelector('#search-from')
+var weatherDayCityEl = document.querySelector('#weather-day-city')
+var weatherDayTempEl = document.querySelector('#weather-day-temp')
+
+
+
+function addeventListeners()
